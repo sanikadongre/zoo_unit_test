c90ef2275341b7b1e85f4e6d5b2bb0afec8c5f31
Sanika Dongre <sado9543@colorado.edu>
2019-12-12 00:43:54 -0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -24,185 +24,159 @@ def getAnimal():
 	return animal
 
 def test_animal_wakeUp(getAnimal, unittest.TestCase):
-	getAnimal.wakeUp()
-	self.assertEqual("name The animal wakes up.\n")
+	ani = getAnimal.wakeUp()
+	self.assertEqual(ani, "name The animal wakes up.\n")
 
-def test_animal_sleep(getAnimal, capsys):
-	getAnimal.sleep()
-	out, err = capsys.readouterr()
-	assert out == "name The animal sleeps.\n"
+def test_animal_sleep(getAnimal, unittest.TestCase):
+	ani = getAnimal.sleep()
+	 self.assertEqual(ani,"name The animal sleeps.\n")
 
-def test_animal_eat(getAnimal, capsys):
-	getAnimal.eat()
-	out, err = capsys.readouterr()
-	assert out == "name The animal eats.\n"
+def test_animal_eat(getAnimal, unittest.TestCase):
+	ani = getAnimal.eat()
+	self.assertEqual(ani,"name The animal eats.\n")
 
-def test_animal_roam(getAnimal, capsys):
-	getAnimal.roam()
-	out, err = capsys.readouterr()
-	assert out == ""
-def test_animal_makeNoise(getAnimal, capsys):
-	getAnimal.makeNoise()
-	out, err = capsys.readouterr()
-	assert out == ""
+def test_animal_roam(getAnimal, unittest.TestCase):
+	ani = getAnimal.roam()
+	self.assertEqual("")
+def test_animal_makeNoise(getAnimal, unittest.TestCase):
+	ani = getAnimal.makeNoise()
+	self.assertEqual("")
 
-@pytest.fixture()
+@classmethod
 def getCanine():
 	canine = Canine("name")
 	return canine
 
-def test_canine_eat(getCanine, capsys):
-	getCanine.eat()
-	out, err = capsys.readouterr()
-	assert out == "name The Canine animal eats meat.\n"
+def test_canine_eat(getCanine, unittest.TestCase):
+	ani = getCanine.eat()
+	self.assertEqual(ani,"name The Canine animal eats meat.\n")
 
-def test_canine_roam(getCanine, capsys):
-	getCanine.roam()
-	out, err = capsys.readouterr()
-	assert out == "name The Canine animal roams in pack.\n"
+def test_canine_roam(getCanine, unittest.TestCase):
+	ani = getCanine.roam()
+	self.assertEqual(ani,"name The Canine animal roams in pack.\n")
 
-@pytest.fixture()
+@classmethod
 def getFeline():
 	feline = Feline("name")
 	return feline
 
-def test_feline_eat(getFeline, capsys):
-	getFeline.eat()
-	out, err = capsys.readouterr()
-	assert out == "name The Feline animal eats raw meat.\n"
+def test_feline_eat(getFeline, unittest.TestCase):
+	ani = getFeline.eat()
+	self.asserEqual(ani,"name The Feline animal eats raw meat.\n")
 
-def test_feline_roam(getFeline, capsys):
-	getFeline.roam()
-	out, err = capsys.readouterr()
-	assert out == "name The Feline animal roams individually/alone.\n"
+def test_feline_roam(getFeline, unittest.TestCase):
+	ani = getFeline.roam()
+	 self.assertEqual(ani,"name The Feline animal roams individually/alone.\n")
 
-@pytest.fixture()
+@classmethod
 def getPachyderm():
 	pachyderm = Pachyderm("name")
 	return pachyderm
 
-def test_pachyderm_eat(getPachyderm, capsys):
-	getPachyderm.eat()
-	out, err = capsys.readouterr()
-	assert out == "name The Pachyderm animal eats grass and/or some meat.\n"
+def test_pachyderm_eat(getPachyderm, unittest.TestCase):
+	ani = getPachyderm.eat()
+	self.assertEqual(ani,"name The Pachyderm animal eats grass and/or some meat.\n")
 
-def test_pachyderm_roam(getPachyderm, capsys):
-	getPachyderm.roam()
-	out, err = capsys.readouterr()
-	assert out == "name The Pachyderm animal roams in herds or by itself.\n"
+def test_pachyderm_roam(getPachyderm, unittest.TestCase):
+	ani = getPachyderm.roam()
+	self.assertEqual(ani,"name The Pachyderm animal roams in herds or by itself.\n")
 
-@pytest.fixture()
+@classmethod
 def getCat():
 	cat = Cat("name")
 	return cat
 
-def test_cat_makeNoise(getCat, capsys):
-	getCat.makeNoise()
-	out, err = capsys.readouterr()
-	assert out == "name Cat makes noise Meow.\n" or out == "name Cat makes noise Meowwww and Meowwww.\n"
-def test_cat_roam(getCat, capsys):
-	getCat.roam()
-	out, err = capsys.readouterr()
-	assert out == "name Cat exercises by stretching and licking itself.\n" or out == "name Cat exercises by playing with itself and purring.\n"
+def test_cat_makeNoise(getCat, unittest.TestCase):
+	ani = getCat.makeNoise()
+	self.assertEqual(ani,"name Cat makes noise Meow.\n" or out == "name Cat makes noise Meowwww and Meowwww.\n")
+def test_cat_roam(getCat, unittest.TestCase):
+	ani = getCat.roam()
+	self.assertEqual(ani,"name Cat exercises by stretching and licking itself.\n") or self.assertEqual(ani,"name Cat exercises by playing with itself and purring.\n")
 
-@pytest.fixture()
+@classmethod
 def getElephant():
 	elephant = Elephant("name")
 	return elephant
-def test_elephant_makeNoise(getElephant, capsys):
-	getElephant.makeNoise()
-	out, err = capsys.readouterr()
-	assert out == "name Elephant makes noise Mooooo.\n"
+def test_elephant_makeNoise(getElephant, unittest.TestCase):
+	ani = getElephant.makeNoise()
+	self.assertEqual(ani,"name Elephant makes noise Mooooo.\n")
 def test_elephant_roam(getElephant, capsys):
-	getElephant.roam()
-	out, err = capsys.readouterr()
-	assert out == "name Elephant exercises by moving and bathing in the muddy water.\n"
+	ani = getElephant.roam()
+	self.assertEqual(ani,"name Elephant exercises by moving and bathing in the muddy water.\n")
 
-@pytest.fixture()
+@classmethod
 def getHippo():
 	hippo = Hippo("name")
 	return hippo
-def test_hippo_makeNoise(getHippo, capsys):
-	getHippo.makeNoise()
-	out, err = capsys.readouterr()
-	assert out == "name Hippo makes noise Hipooo.\n"
+def test_hippo_makeNoise(getHippo, unittest.TestCase):
+	ani = getHippo.makeNoise()
+	self.assertEqual(ani,"name Hippo makes noise Hipooo.\n")
 
-def test_hippo_roam(getHippo, capsys):
-	getHippo.roam()
-	out, err = capsys.readouterr()
-	assert out == "name Hippo exercises by jumping up and down in the water.\n"
+def test_hippo_roam(getHippo, unittest.TestCase):
+	ani = getHippo.roam()
+	self.assertEqual(ani,"name Hippo exercises by jumping up and down in the water.\n")
 
-@pytest.fixture()
+@classmethod
 def getLion():
 	lion = Lion("name")
 	return lion
-def test_lion_makeNoise(getLion, capsys):
+def test_lion_makeNoise(getLion, unittest.TestCase):
 	getLion.makeNoise()
-	out, err = capsys.readouterr()
-	assert out == "name Lion makes noise LionGrrr.\n"
-def test_lion_roam(getLion, capsys):
-	getLion.roam()
-	out, err = capsys.readouterr()
-	assert out == "name Lion exercises by running quick and putting claws in trees.\n"
+	self.assertEqual(ani,"name Lion makes noise LionGrrr.\n")
+def test_lion_roam(getLion, unittest.TestCase):
+	ani = getLion.roam()
+	self.assertEqual(ani,"name Lion exercises by running quick and putting claws in trees.\n")
 
-@pytest.fixture()
+@classmethod
 def getRhino():
 	rhino = Rhino("name")
 	return rhino
 
-def test_rhino_makeNoise(getRhino, capsys):
-	getRhino.makeNoise()
-	out, err = capsys.readouterr()
-	assert out == "name Rhino makes noise Rhinooo.\n"
-def test_rhino_roam(getRhino, capsys):
-	getRhino.roam()
-	out, err = capsys.readouterr()
-	assert out == "name Rhino exercises by jumping up and down in the grass.\n"
+def test_rhino_makeNoise(getRhino, unittest.TestCase):
+	ani = getRhino.makeNoise()
+	self.assertEqual(ani,"name Rhino makes noise Rhinooo.\n")
+def test_rhino_roam(getRhino, unittest.TestCase):
+	ani = getRhino.roam()
+	self.assertEqual(ani,"name Rhino exercises by jumping up and down in the grass.\n")
 
-@pytest.fixture()
+@classmethod
 def getTiger():
 	tiger = Tiger("name")
 	return tiger
 
-def test_tiger_makeNoise(getTiger, capsys):
-	getTiger.makeNoise()
-	out, err = capsys.readouterr()
-	assert out == "name Tiger makes noise Tigrrrrr.\n"
+def test_tiger_makeNoise(getTiger, unittest.TestCase):
+	ani = getTiger.makeNoise()
+	self.assertEqual(ani,"name Tiger makes noise Tigrrrrr.\n")
 def test_tiger_roam(getTiger, capsys):
 	getTiger.roam()
-	out, err = capsys.readouterr()
-	assert out == "name Tiger exercises by running quick and putting claws in trees.\n"
+	self.assertEqual(ani,"name Tiger exercises by running quick and putting claws in trees.\n")
+	
 
-@pytest.fixture()
+@classmethod
 def getWolf():
 	wolf = Wolf("name")
 	return wolf
 
-def test_wolf_makeNoise(getWolf, capsys):
-	getWolf.makeNoise()
-	out, err = capsys.readouterr()
-	assert out == "name Wolf makes noise Grrrrr.\n"
-def test_wolf_roam(getWolf, capsys):
+def test_wolf_makeNoise(getWolf, unittest.TestCase):
+	ani = getWolf.makeNoise()
+	self.assertEqual(ani,"name Wolf makes noise Grrrrr.\n")
+def test_wolf_roam(getWolf, unittest.TestCase):
 	getWolf.roam()
-	out, err = capsys.readouterr()
-	assert out == "name Wolf exercises by stretching and licking itself.\n"
+	self.assertEqual(ani,"name Wolf exercises by stretching and licking itself.\n")
 
-@pytest.fixture()
+@classmethod
 def getDog():
 	dog = Dog("name")
 	return dog
-def test_dog_makeNoise(getDog, capsys):
-	getDog.makeNoise()
-	out, err = capsys.readouterr()
-	assert out == "name Dog makes noise Woof.\n"
-def test_dog_roam(getDog, capsys):
-	getDog.roam()
-	out, err = capsys.readouterr()
-	assert out == "name Dog exercises by stretching and sprinting.\n"
+def test_dog_makeNoise(getDog, unittest.TestCase):
+	ani = getDog.makeNoise()
+	self.assertEqual(ani,"name Dog makes noise Woof.\n")
+def test_dog_roam(getDog, unittest.TestCase):
+	ani = getDog.roam()
+	self.assertEqual(ani,"name Dog exercises by stretching and sprinting.\n")
 
 
 def test_zookeeper_init(capsys):
-	ZooKeeper()
-	out, err = capsys.readouterr()
-	assert out == """----- Day At the Zoo: Output -----
-Zoo Opened.
+	ani = ZooKeeper()
+	self.assertEqual(ani,"""----- Day At the Zoo: Output -----
+Zoo Opened.)
